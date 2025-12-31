0ca257cc49bf4ce307d3487b0e89d580aed1fa4c
TechHead25 <shrishpatil678@gmail.com>
2025-12-31 17:32:35 +0530

    Update Fireworks.jsx

--- src/components/Fireworks.jsx
+++ src/components/Fireworks.jsx
@@ -1,18 +1,7 @@
 import { useEffect } from "react";
 import Lottie from "lottie-react";
-import fireworks from "../assets/fireworks.json";
 
 export default function Fireworks({ show, onEnd }) {
-  useEffect(() => {
-    if (show) {
-      const timer = setTimeout(() => {
-        onEnd();
-      }, 6000); // stop after 6s
-
-      return () => clearTimeout(timer);
-    }
-  }, [show, onEnd]);
-
   if (!show) return null;
 
   return (
@@ -22,14 +11,45 @@ export default function Fireworks({ show, onEnd }) {
         inset: 0,
         zIndex: 50,
         pointerEvents: "none",
-        background: "transparent",
       }}
     >
       <Lottie
-        animationData={fireworks}
+        path="/fireworks.json"
         loop={false}
-        style={{ width: "100%", height: "100%" }}
       />
     </div>
   );
 }
+
+
+// export default function Fireworks({ show, onEnd }) {
+//   useEffect(() => {
+//     if (show) {
+//       const timer = setTimeout(() => {
+//         onEnd();
+//       }, 6000); // stop after 6s
+
+//       return () => clearTimeout(timer);
+//     }
+//   }, [show, onEnd]);
+
+//   if (!show) return null;
+
+//   return (
+//     <div
+//       style={{
+//         position: "fixed",
+//         inset: 0,
+//         zIndex: 50,
+//         pointerEvents: "none",
+//         background: "transparent",
+//       }}
+//     >
+//       <Lottie
+//         animationData={fireworks}
+//         loop={false}
+//         style={{ width: "100%", height: "100%" }}
+//       />
+//     </div>
+//   );
+// }
